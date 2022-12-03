4b93cc3fc9cfbbd7c560cd1d415d7d1e63b12dd6
jeffersonkako <jefferson@kako.dev>
2022-12-03 18:18:18 -0300

    V5

--- Dados/bd.py
+++ Dados/bd.py
@@ -1,2 +1,41 @@
 import pandas as pd
 
+def DadosFerramentas():
+    dadosF = {
+        'FERRAMENTA': [],
+        'MODELO': [],
+        'FABRICANTE': [],
+        'PESO': [],
+        'QUANTIDADE': [],
+        'VOLTAGEM': [],
+        'TIPO': [],
+    }
+
+    dadosF_df = pd.DataFrame(dadosF)
+    dadosFerramentas = pd.read_excel('ferramentas.xlsx')
+    print(dadosFerramentas)
+
+def DadosTecnicos():
+    dadosT = {
+        'TECNICOS': [],
+        'FERRAMENTAS': [],
+        'DATA DA RESERVA': [],
+        'DATA DA DEVOLUÇÃO': [],
+    }
+
+    dadosT_df = pd.DataFrame(dadosT)
+    dadosTecnicos = pd.read_excel('tecnicos.xlsx')
+    print(dadosTecnicos)
+
+def DadosResevas():
+    dadosR = {
+        'TECNICOS': [],
+        'FERRAMENTAS': [],
+        'DATA DA RESERVA': [],
+        'DATA DA DEVOLUÇÃO': [],
+    }
+
+    dadosR_df = pd.DataFrame(dadosR)
+    dadosReservas = pd.read_excel('reservas.xlsx')
+    print(dadosReservas)
+
