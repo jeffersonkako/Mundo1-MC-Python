47212b02a2e5c64ebee34a62413be48e65941f1c
jeffersonkako <jefferson@kako.dev>
2022-12-03 20:25:15 -0300

    V6

--- Telas/Janelas.py
+++ Telas/Janelas.py
@@ -12,13 +12,24 @@ def sg_Login():
         [sg.Text('Senha')],
         [sg.Input(key='senha')],
         [sg.Text('', key='mensagem')],
-        [sg.Button('Login', button_color='#5c2fd8'), sg.Button('Sair', button_color='#a0a0a0')],
+        [sg.Button('Login', button_color='#5c2fd8'), 
+        sg.Button('Sair', button_color='#a0a0a0'), sg.Text(),
+        sg.Button('Equipe', button_color='#a0a0a0')
+        ],
     ]
     
     janelaLogin = sg.Window('LOGIN', layout=layoutLogin, font='Roboto 15')
 
     while True:
         event, values = janelaLogin.read()
+        if event == 'Equipe':
+            Team2_inf = DadosEquipe()
+            sg.popup(Team2_inf,
+            font='Roboto 20', 
+            background_color='#5c2fd8', 
+            auto_close=True, 
+            auto_close_duration=10,
+            no_titlebar=True)
         if event == 'Sair':
             sg.Exit()
             break
@@ -104,14 +115,14 @@ def sg_CadastroGeral():
 # TELA DO CADASTRO DE FERRAMENTAS --------------------------------------------------------------
 def sg_CadastroF():
     layoutCadF = [ 
-            [sg.Text('Ferramenta'), sg.InputText()],
-            [sg.Text('Modelo'), sg.InputText()],
-            [sg.Text('Fabricante'), sg.InputText()],
-            [sg.Text('Descrição'), sg.InputText()],
-            [sg.Text('Peso'), sg.InputText()],
-            [sg.Text('Quantidade'), sg.InputText()],
-            [sg.Text('Voltagem'), sg.InputText()],
-            [sg.Text('Tipo'), sg.InputText()],
+            [sg.Text('Ferramenta'), sg.InputText(key='-Ferramenta-')],
+            [sg.Text('Modelo'), sg.InputText(key='-Modelo-')],
+            [sg.Text('Fabricante'), sg.InputText(key='-Fabricante-')],
+            [sg.Text('Descrição'), sg.InputText(key='-Descrição-')],
+            [sg.Text('Peso'), sg.InputText(key='-Peso-')],
+            [sg.Text('Quantidade'), sg.InputText(key='-Quantidade-')],
+            [sg.Text('Voltagem'), sg.InputText(key='-Voltagem-')],
+            [sg.Text('Tipo'), sg.InputText(key='-Tipo-')],
             [sg.Button('Cadastrar'), sg.Button('Voltar', button_color='#a0a0a0')] 
             ]
 
@@ -120,11 +131,12 @@ def sg_CadastroF():
     while True:
         event, values = janelaCadastroF.read()
         if event == 'Cadastrar':
-            sg.popup('Ferramenta Cadastrada', 
+            Ferramentas_inf = DadosFerramentas(values)
+            sg.popup(Ferramentas_inf,
             font='Roboto 20', 
             background_color='#007d4d', 
             auto_close=True, 
-            auto_close_duration=2,
+            auto_close_duration=10,
             no_titlebar=True)
             janelaCadastroF.close()
             sg_CadastroF()
@@ -139,10 +151,10 @@ def sg_CadastroF():
 # TELA DO CADASTRO DE TÉCNICOS -----------------------------------------------------------------
 def sg_CadastroT():
     layoutCadT = [ 
-            [sg.Text('CPF'), sg.InputText()],
-            [sg.Text('Nome'), sg.InputText()],
-            [sg.Text('Sobrenome'), sg.InputText()],
-            [sg.Text('Telefone'), sg.InputText()],
+            [sg.Text('CPF'), sg.InputText(key='-CPF-')],
+            [sg.Text('Nome'), sg.InputText(key='-Nome-')],
+            [sg.Text('Sobrenome'), sg.InputText(key='-Sobrenome-')],
+            [sg.Text('Telefone'), sg.InputText(key='-Telefone-')],
             [sg.Text('Turno: '), sg.Radio('Manhã', 'RADIO', key='-MANHA-'), sg.Radio('Noite', 'RADIO', key='-Noite-')],
             [sg.Text('Equipe: '), 
             sg.Radio('Team 1', 'RADIO', key='-TEAM1-'), 
@@ -157,11 +169,12 @@ def sg_CadastroT():
     while True:
         event, values = janelaCadastroT.read()
         if event == 'Cadastrar':
-            sg.popup('Técnico Cadastrado', 
+            Tecnico_inf = DadosTecnico(values)
+            sg.popup(Tecnico_inf,
             font='Roboto 20', 
             background_color='#007d4d', 
             auto_close=True, 
-            auto_close_duration=2,
+            auto_close_duration=10,
             no_titlebar=True)
             janelaCadastroT.close()
             sg_CadastroT()
@@ -176,8 +189,8 @@ def sg_CadastroT():
 # TELA DO CADASTRO DE RESERVAS ---------------------------------------------------------------------------------------------------------------------------------------------------------
 def sg_CadastroR():
     layoutCadR = [ 
-            [sg.Text('Tecnico'), sg.InputText()],
-            [sg.Text('Ferramenta', key='-FERRAMENTA-'), sg.InputText()],
+            [sg.Text('Tecnico'), sg.InputText(key='-TECNICO-')],
+            [sg.Text('Ferramenta'), sg.InputText(key='-FERRAMENTA-')],
             [sg.Input(key='DT-RESERVA', size=(20,1)) , sg.CalendarButton('Data da Reserva', close_when_date_chosen=True, target='DT-RESERVA',location=(800,400),no_titlebar=False)],
             [sg.Input(key='DT-DEVOLUCAO', size=(20,1)), sg.CalendarButton('Data da Devolução', close_when_date_chosen=True, target='DT-DEVOLUCAO',location=(800,400),no_titlebar=False)],
             [sg.Button('Cadastrar'), sg.Button('Voltar', button_color='#a0a0a0')] 
@@ -188,11 +201,12 @@ def sg_CadastroR():
     while True:
         event, values = janelaCadastroR.read()
         if event == 'Cadastrar':
-            sg.popup('Reserva Cadastrada', 
+            Reservas_inf = DadosReserva(values)
+            sg.popup(Reservas_inf,
             font='Roboto 20', 
             background_color='#007d4d', 
             auto_close=True, 
-            auto_close_duration=2,
+            auto_close_duration=10,
             no_titlebar=True)
             janelaCadastroR.close()
             sg_CadastroR()
@@ -265,4 +279,65 @@ def sg_Editar():
             sg_Menu()
         elif event == sg.WIN_CLOSED:
             break
+
+
+# DADOS INPUTS ---------------------------------------------------------------------------------------------
+def DadosReserva(values):
+    info = 'RESERVA CADASTRADA\n'
+    nome = '\nNome: '+ values['-TECNICO-']
+    info += nome
+    ferramentaR = '\nFerramenta: '+ values['-FERRAMENTA-']
+    info += ferramentaR
+    dataR = '\nReserva: ' + values['DT-RESERVA']
+    info += dataR
+    dataD = '\nDevolução: ' + values['DT-DEVOLUCAO']
+    info += dataD
+    return info
+
+
+def DadosFerramentas(values):
+    info = 'FERRAMENTA CADASTRADA\n'
+    Ferramenta = '\nFerramenta: '+ values['-Ferramenta-']
+    info += Ferramenta
+    Fabricante = '\nFabricante: '+ values['-Fabricante-']
+    info += Fabricante
+    Descrição = '\nDescrição: ' + values['-Descrição-']
+    info += Descrição
+    Peso = '\nPeso: ' + values['-Peso-']
+    info += Peso
+    Quantidade = '\nQuantidade: ' + values['-Quantidade-']
+    info += Quantidade
+    Voltagem = '\nVoltagem: ' + values['-Voltagem-']
+    info += Voltagem
+    Tipo = '\nTipo: ' + values['-Tipo-']
+    info += Tipo
+    return info
+
+
+def DadosTecnico(values):
+    info = 'Técnico Cadastrado\n'
+    CPF = '\nCPF: '+ values['-CPF-']
+    info += CPF
+    Nome = '\nNome: '+ values['-Nome-']
+    info += Nome
+    Sobrenome = '\nSobrenome: ' + values['-Sobrenome-']
+    info += Sobrenome
+    Telefone = '\nTelefone: ' + values['-Telefone-']
+    info += Telefone
+    return info
+
+def DadosEquipe():
+    info = 'TEAM 2\n'
+    Aluno1 = '\nJefferson'
+    info += Aluno1
+    Aluno2 = '\nAmandio'
+    info += Aluno2
+    Aluno3 = '\nAriel'
+    info += Aluno3
+    Aluno4 = '\nRaiza'
+    info += Aluno4
+    return info
+
+
+
 
